2b1c57e772e82864cca37e3a064cb45e7cdd87c0
Katherine Martin <katie.a.martin.18@gmail.com>
2021-07-25 23:28:01 -0700

    are you working

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from V2</p>
+        <p>Are you working</p>
       </header>
     </div>
   );
