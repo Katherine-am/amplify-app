ace5d284ad250cfb2f6c6eb8c1d0383b786f41a4
Katherine Martin <katie.a.martin.18@gmail.com>
2021-07-30 00:14:34 -0700

    reverting back to proper file structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './App.css';
-import Header from './components/Organisms_2/Header';
+import Header from './components/Organisms/Header';
 import Main from './components/Organisms/Main';
 import Footer from './components/Organisms/Footer';
 
