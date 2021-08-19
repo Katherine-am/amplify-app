5d07b5786e5b3a57fafb586f04a07fc1915296c5
Katherine Martin <katie.a.martin.18@gmail.com>
2021-08-18 19:31:13 -0700

    reinstating commented out components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Organisms/Header';
 import Main from './components/Organisms/Main';
-// import Footer from './components/Organisms/Footer';
+import Footer from './components/Organisms/Footer';
 
 function App() {
   return (
     <div>
-      {/* <Header />
+      <Header />
       <Main />
-      <Footer /> */}
-      <p>checking for updates without calling folder structure</p>
+      <Footer /> 
     </div>
   );
 }
