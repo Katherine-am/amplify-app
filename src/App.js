06928e23ac14044b97b33b8882c01327e88adb18
Katherine Martin <katie.a.martin.18@gmail.com>
2021-08-18 19:01:14 -0700

    commented out imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import './App.css';
-import Header from './components/Organisms/Header';
-import Main from './components/Organisms/Main';
-import Footer from './components/Organisms/Footer';
+// import Header from './components/Organisms/Header';
+// import Main from './components/Organisms/Main';
+// import Footer from './components/Organisms/Footer';
 
 function App() {
   return (
