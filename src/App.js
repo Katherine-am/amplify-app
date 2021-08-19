307aee60375156d9076f278d7e3b07d4a1ad43f5
Katherine Martin <katie.a.martin.18@gmail.com>
2021-08-18 19:22:44 -0700

    changing component folder name

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.css';
 import Header from './components/Organisms/Header';
-// import Main from './components/Organisms/Main';
+import Main from './components/Organisms/Main';
 // import Footer from './components/Organisms/Footer';
 
 function App() {
