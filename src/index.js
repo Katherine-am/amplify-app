c7fc5d93d6e48292e4274c4bf91a30cdede36067
Katherine Martin <katie.a.martin.18@gmail.com>
2021-09-25 14:52:17 -0700

    fixed quotes around aws-exports import in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.scss';
 import App from './App';
 
 import Amplify from 'aws-amplify';
-import awsExports from "./aws-exports";
+import awsExports from './aws-exports.js';
 Amplify.configure(awsExports);
 
 ReactDOM.render(
